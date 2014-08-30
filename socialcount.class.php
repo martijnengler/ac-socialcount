fe2bfa7e09e82a965302de6b169905fdb4b33f01
Martijn Engler <git@martijnengler.nl>
2014-08-30 13:27:20 +0200

    create a response object in AC_SocialCount

--- socialcount.class.php
+++ socialcount.class.php
@@ -4,11 +4,13 @@ class AC_SocialCount
   protected $_url       = "";
   protected $_score     = 0;
   protected $_weighted  = true;
+  protected $_response  = null;
 
   public function __construct($url, $weighted = true)
   {
     $this->_url       = $url;
     $this->_weighted  = $weighted;
+    $this->_response  = new ACSC_Response($this->_url);
   }
 
   public function calculate_score()
