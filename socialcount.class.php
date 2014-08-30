96c4d3813c3b4f1227daaa1e5b7ff4bcce90e68a
Martijn Engler <git@martijnengler.nl>
2014-08-30 12:58:21 +0200

    make weighted a class attribute, instead of argument
    weighted makes more sense as an attribute than an argument
    
    add "weighted" parameter to constructor

--- socialcount.class.php
+++ socialcount.class.php
@@ -1,19 +1,21 @@
 <?php
 class AC_SocialCount
 {
-  protected $_url   = "";
-  protected $_score = 0;
+  protected $_url       = "";
+  protected $_score     = 0;
+  protected $_weighted  = true;
 
-  public function __construct($url)
+  public function __construct($url, $weighted = true)
   {
-    $this->_url = $url;
+    $this->_url       = $url;
+    $this->_weighted  = $weighted;
   }
 
   public function calculate_score()
   {
   }
 
-  public function get_score($weighted = true, $force_calc = false)
+  public function get_score($force_calc = false)
   {
     return $this->_score;
   }
