e09a972e2f86a0d1109eed8db2f04a650310eb7f
Martijn Engler <git@martijnengler.nl>
2014-08-30 14:13:03 +0200

    calculating scores without issue

--- socialcount.class.php
+++ socialcount.class.php
@@ -8,18 +8,22 @@ class AC_SocialCount
 
   public function __construct($url, $weighted = true)
   {
+    $response = new ACSC_Response($this->_url);
+
     $this->_url       = $url;
     $this->_weighted  = $weighted;
-    $this->_scores    = new ACSC_Response($this->_url);
+    $this->_scores    = $response->get_response();
   }
 
   public function calculate_score()
   {
-
+    // Facebook scores are divided into likes, comments etc. so we need to add those up separately
+    $this->_score = (array_sum($this->_scores) + $this->_scores["Facebook"]["total_count"]);
   }
 
   public function get_score($force_uncached = false)
   {
+    $this->calculate_score();
     return $this->_score;
   }
 }
