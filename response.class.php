e717dc6ef7b161058ea90bb79e2e8801476b5c29
Martijn Engler <git@martijnengler.nl>
2014-08-30 13:13:22 +0200

    do a very basic implementation of fetch() method

--- response.class.php
+++ response.class.php
@@ -1,19 +1,24 @@
 <?php
 class ACSC_Response
 {
-  protected $_url       = "";
+  protected $_url           = "";
+  protected $_raw_response  = "";
+  protected $_response      = "";
 
   public function __construct($url)
   {
     $this->_url       = $url;
+    $this->fetch();
+    $this->process();
   }
 
   protected function fetch()
   {
-
+    $api_url  = sprintf(AC_SC_API_HOST . "?api_key=%s&url=%s", AC_SC_API_KEY, urlencode($this->_url));
+    $this->_raw_response = wp_remote_get($api_url);
   }
 
-  protected function process_response()
+  protected function process()
   {
   }
 
