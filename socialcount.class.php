79dfcb49fc62d4590d4172ad24ccc4151aa84ce7
Martijn Engler <git@martijnengler.nl>
2014-08-30 14:04:02 +0200

    reword some vars for clarity
    rename _response to _scores for AC_SocialCount
    
    rename $force_calc to $force_uncached for clarity

--- socialcount.class.php
+++ socialcount.class.php
@@ -4,20 +4,21 @@ class AC_SocialCount
   protected $_url       = "";
   protected $_score     = 0;
   protected $_weighted  = true;
-  protected $_response  = null;
+  protected $_scores    = null;
 
   public function __construct($url, $weighted = true)
   {
     $this->_url       = $url;
     $this->_weighted  = $weighted;
-    $this->_response  = new ACSC_Response($this->_url);
+    $this->_scores    = new ACSC_Response($this->_url);
   }
 
   public function calculate_score()
   {
+
   }
 
-  public function get_score($force_calc = false)
+  public function get_score($force_uncached = false)
   {
     return $this->_score;
   }
