9163183daa3e54f5fbcc8769648d85c51c4d7740
Martijn Engler <git@martijnengler.nl>
2014-08-30 14:16:34 +0200

    create a table interface for backend

--- admin.php
+++ admin.php
@@ -1,11 +1,30 @@
 <?php
-$posts = array();
- 
-// fetch posts via Wp_Auery magic
-foreach($posts as $post)
+$the_query = new WP_Query('post_type=post&post_status=publish&posts_per_page=50');
+if($the_query->have_posts())
 {
-  // $permalink = get_permalink($post);
-  // $counts[$post->ID] = get_social_count($post);
+	while($the_query->have_posts())
+  {
+    $the_query->the_post();
+    $count = new AC_SocialCount('https://applecoach.nl/pomodoro-one-mac');
+    $counts[get_the_ID()] = array("title" => get_the_title(), "permalink" => get_the_permalink(), "score" => $count->get_score());
+  }
 }
-// natsort($counts);
-// display first natsort
+
+wp_reset_postdata();
+
+uasort($counts, function($a,$b){
+  $a = $a["score"];
+  $b = $b["score"];
+  if ($a == $b) {
+      return 0;
+  }
+  return ($a > $b) ? -1 : 1;
+});
+
+echo "<table>";
+echo '<thead><tr><th>Post title</th><th>Score</th></tr></thead><tbody>';
+foreach($counts as $c)
+{
+  printf('<tr><td><a href="%s">%s</a></td><td>%s</td></tr>', $c["permalink"], $c["title"], $c["score"]);
+}
+echo '</tbody></table>';
