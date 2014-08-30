ce4e88eb948cda5d5fa261f61a4d0e8d34dae03c
Martijn Engler <git@martijnengler.nl>
2014-08-30 10:17:15 +0200

    first commit, quick draft via vim on iPad

---
+++ ac-socialcount.php
@@ -0,0 +1,9 @@
+<?php
+// fetch posts via Wp_Auery magic
+foreach($posts as $post)
+{
+  $permalink = get_permalink($post);
+  // $counts[$post->ID] = get_social_count($post);
+}
+natsort($counts);
+// display first natsort
