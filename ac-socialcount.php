78701502dbda55443ae127b5d08144c526e0a9dd
Martijn Engler <git@martijnengler.nl>
2014-08-30 10:22:21 +0200

    add a WP Plugin description

--- ac-socialcount.php
+++ ac-socialcount.php
@@ -1,4 +1,15 @@
 <?php
+<?php
+/*
+Plugin Name: AC Social Counter
+Description: Uses the SharedCount.com API to define the most popular blogs of a WordPress install.
+
+Needs a (free) API key from sharedcount.com
+Version: 1.0
+Author: Martijn Engler
+Author URI: http://applecoach.nl/
+*/
+
 // fetch posts via Wp_Auery magic
 foreach($posts as $post)
 {
